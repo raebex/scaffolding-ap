1b3758cebfedbf9b49425b9752195d5abec59aee
rebecca <r.young1025@gmail.com>
2021-02-23 21:23:17 -0600

    scaffolding tutorial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,21 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @posts = Post.all
+
+    respond_to do |format|
+      format.html  # index.html.erb
+      format.json  { render :json => @posts }
+    end
   end
 
   # GET /posts/1 or /posts/1.json
   def show
+    @post = Post.find(params[:id])
+ 
+    respond_to do |format|
+      format.html  # show.html.erb
+      format.json  { render :json => @post }
+    end
   end
 
   # GET /posts/new
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  validates :name,  :presence => true
+  validates :title, :presence => true
+  validates :content, :length => { :minimum => 5 }
+
+  has_many :comments
 end
